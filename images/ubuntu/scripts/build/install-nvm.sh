6ba2785e134bfc406d8dda1c3a76369333b93d04
Rahul Vishwakarma <Rahul.Vishwakarma2@ibm.com>
2026-01-21 17:17:06 +0530

    build(nvm): make install script idempotent
    Signed-off-by: Rahul Vishwakarma <Rahul.Vishwakarma2@ibm.com>

--- images/ubuntu/scripts/build/install-nvm.sh
+++ images/ubuntu/scripts/build/install-nvm.sh
@@ -9,14 +9,20 @@
 source "$HELPER_SCRIPTS"/etc-environment.sh
 
 export NVM_DIR="/etc/skel/.nvm"
-mkdir "$NVM_DIR"
-nvm_version=$(curl -fsSL https://api.github.com/repos/nvm-sh/nvm/releases/latest | jq -r '.tag_name')
-curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/"$nvm_version"/install.sh | bash
+mkdir -p "$NVM_DIR"
+
+if [ ! -f "$NVM_DIR/nvm.sh" ]; then
+    nvm_version=$(curl -fsSL https://api.github.com/repos/nvm-sh/nvm/releases/latest | jq -r '.tag_name')
+    curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/"$nvm_version"/install.sh | bash
+else
+    echo "NVM already installed at $NVM_DIR. Skipping installation."
+fi
+
 # shellcheck disable=SC2016
 set_etc_environment_variable "NVM_DIR" '$HOME/.nvm'
 
 # shellcheck disable=SC2016
-echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' | tee -a /etc/skel/.bash_profile
+grep -qF "nvm.sh" /etc/skel/.bash_profile || echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' | tee -a /etc/skel/.bash_profile
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 # set system node.js as default one
