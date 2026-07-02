0e7a3a46e1b8bf361b580b07d4b0374f73781a50
Rahul Vishwakarma <Rahul.Vishwakarma2@ibm.com>
2026-07-02 13:06:02 +0530

    fix(kata-containers): install apt-utils before other packages to avoid debconf issues
    Signed-off-by: Rahul Vishwakarma <Rahul.Vishwakarma2@ibm.com>

--- images/ubuntu/scripts/build/install-apt-vital.sh
+++ images/ubuntu/scripts/build/install-apt-vital.sh
@@ -8,5 +8,8 @@
 # shellcheck disable=SC1091
 source "$HELPER_SCRIPTS"/install.sh
 
+# apt-utils must be present before any other package triggers debconf configuration
+install_dpkgs apt-utils
+
 vital_packages=$(get_toolset_value .apt.vital_packages[])
 install_dpkgs --no-install-recommends "$vital_packages"
